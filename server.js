8fb86d34308f4ca9fb9183cdd11f9a160d0c45f3
Viktor Lazarov <viktorlazarov@abv.bg>
2024-06-13 09:06:34 +0200

    downloaded photos for the scroller animation, changed db connection to .env file

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ dotev.config();
 const  db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '074624750a',
-        database: 'loginform'
-        // host: process.env.DB_HOST,
-        // user: process.env.DB_USER,
-        // password: String(process.env.DB_PASSWORD),
-        // database: process.env.DB_DATABASE
+        // host: '127.0.0.1',
+        // user: 'postgres',
+        // password: '074624750a',
+        // database: 'loginform'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: String(process.env.DB_PASSWORD),
+        database: process.env.DB_DATABASE
     }
 })
 
